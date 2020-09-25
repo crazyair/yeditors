f1e08367e99f692932482f28185ff61a0ede5238
黄建峰 <645381995@qq.com>
2020-09-25 16:12:19 +0800

    修复 build

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,4 +1,8 @@
-export default {
-  esm: 'rollup',
-  cjs: 'rollup',
+const options = {
+  runtimeHelpers: true,
+  cjs: { type: 'babel', lazy: true },
+  cssModules: false, // https://github.com/umijs/father/issues/131
+  lessInBabelMode: true,
 };
+
+export default options;
---
+++ docs/examples/index.md
@@ -0,0 +1,10 @@
+---
+title: 示例
+nav:
+  title: Examples
+  order: 3
+---
+
+# 示例
+
+## 基础使用
