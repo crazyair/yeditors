b122dcdb541fc20e90b4729a36d495232ebcdda8
黄建峰 <645381995@qq.com>
2020-10-13 15:37:57 +0800

    feat: 0.0.9

--- .fatherrc.ts
+++
@@ -1,8 +0,0 @@
-const options = {
-  runtimeHelpers: true,
-  cjs: { type: 'babel', lazy: true },
-  cssModules: false, // https://github.com/umijs/father/issues/131
-  lessInBabelMode: true,
-};
-
-export default options;
