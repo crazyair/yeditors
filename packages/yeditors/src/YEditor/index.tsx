1171e627f8d7b4ef2b345d47ce4b1435bf1149bc
黄建峰 <645381995@qq.com>
2020-10-13 14:47:51 +0800

    feat: 测试

---
+++ packages/yeditors/src/YEditor/index.tsx
@@ -0,0 +1,14 @@
+import { serialize } from './utils';
+import Editor from './YEditor';
+
+type InternalYEditor = typeof Editor;
+
+interface RefYEditor extends InternalYEditor {
+  serialize: typeof serialize;
+}
+
+const YEditor: RefYEditor = Editor as RefYEditor;
+
+YEditor.serialize = serialize;
+
+export default YEditor;
--- packages/yeditors/src/index.tsx
+++ packages/yeditors/src/index.tsx
@@ -1,14 +1,1 @@
-import { serialize } from './utils';
-import Editor from './YEditor';
-
-type InternalYEditor = typeof Editor;
-
-interface RefYEditor extends InternalYEditor {
-  serialize: typeof serialize;
-}
-
-const YEditor: RefYEditor = Editor as RefYEditor;
-
-YEditor.serialize = serialize;
-
-export { YEditor };
+export { default as YEditor } from './YEditor';
